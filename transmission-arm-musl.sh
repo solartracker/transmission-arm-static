c9c463ccfb48fed55e390787c17d9e6dc8ccecbb
Richard Elwell <229418400@users.noreply.github.com>
2026-02-10 16:30:24 -0500

    correct sysroot and update shared code

--- transmission-arm-musl.sh
+++ transmission-arm-musl.sh
@@ -31,6 +31,7 @@ set -x
 main() {
 PKG_ROOT=transmission
 PKG_TARGET_CPU=armv7
+PKG_TARGET_VARIANT=
 
 BUILD_TRANSMISSION_VERSION="3.00"
 #BUILD_TRANSMISSION_VERSION="4.0.6+bundled_third_party"
@@ -50,12 +51,13 @@ fi
 CROSSBUILD_SUBDIR="cross-arm-linux-musleabi-build"
 CROSSBUILD_DIR="${PARENT_DIR}/${CROSSBUILD_SUBDIR}"
 export TARGET=arm-linux-musleabi
+TARGET_DIR="${CROSSBUILD_DIR}/${TARGET}"
 
 HOST_CPU="$(uname -m)"
-export PREFIX="${CROSSBUILD_DIR}"
+SYSROOT="${TARGET_DIR}/sysroot"
+export PREFIX="${SYSROOT}"
 export HOST=${TARGET}
-export SYSROOT="${PREFIX}/${TARGET}"
-export PATH="${PATH}:${PREFIX}/bin:${SYSROOT}/bin"
+export PATH="${CROSSBUILD_DIR}/bin:${PATH}"
 
 CROSS_PREFIX=${TARGET}-
 export CC=${CROSS_PREFIX}gcc
@@ -204,7 +206,7 @@ sign_file()
 
     local target_path="$1"
     local option="$2"
-    local sign_path="$(readlink -f "${target_path}").sum"
+    local sum_path="$(readlink -f "${target_path}").sum"
     local target_file="$(basename -- "${target_path}")"
     local target_file_hash=""
     local temp_path=""
@@ -231,7 +233,7 @@ sign_file()
     trap 'cleanup; exit 130' INT
     trap 'cleanup; exit 143' TERM
     trap 'cleanup' EXIT
-    temp_path=$(mktemp "${sign_path}.XXXXXX")
+    temp_path=$(mktemp "${sum_path}.XXXXXX")
     {
         #printf '%s released %s\n' "${target_file}" "${now_localtime}"
         #printf '\n'
@@ -241,8 +243,7 @@ sign_file()
     } >"${temp_path}" || return 1
     chmod --reference="${target_path}" "${temp_path}" || return 1
     touch -r "${target_path}" "${temp_path}" || return 1
-    mv -f "${temp_path}" "${sign_path}" || return 1
-    # TODO: implement signing
+    mv -f "${temp_path}" "${sum_path}" || return 1
     trap - EXIT INT TERM
 
     return 0
@@ -299,66 +300,65 @@ hash_archive()
 verify_hash() {
     [ -n "$1" ] || return 1
 
-    local file_path="$1"
+    local source_path="$1"
     local expected="$2"
     local option="$3"
     local actual=""
-    local sign_path="$(readlink -f "${file_path}").sum"
+    local sum_path="$(readlink -f "${source_path}").sum"
     local line=""
 
-    if [ ! -f "${file_path}" ]; then
-        echo "ERROR: File not found: ${file_path}"
+    if [ ! -f "${source_path}" ]; then
+        echo "ERROR: File not found: ${source_path}"
         return 1
     fi
 
     if [ -z "${option}" ]; then
         # hash the compressed binary archive itself
-        actual="$(sha256sum "${file_path}" | awk '{print $1}')"
+        actual="$(sha256sum "${source_path}" | awk '{print $1}')"
     elif [ "${option}" = "full_extract" ]; then
         # hash the data inside the compressed binary archive
-        actual="$(hash_archive "${file_path}")"
+        actual="$(hash_archive "${source_path}")"
     elif [ "${option}" = "xz_extract" ]; then
         # hash the data, file names, directory names, timestamps, permissions, and
         # tar internal structures. this method is not as "future-proof" for archiving
         # Github repos because it is possible that the tar internal structures
         # could change over time as the tar implementations evolve.
-        actual="$(xz -dc "${file_path}" | sha256sum | awk '{print $1}')"
+        actual="$(xz -dc "${source_path}" | sha256sum | awk '{print $1}')"
     else
         return 1
     fi
 
     if [ -z "${expected}" ]; then
-        if [ ! -f "${sign_path}" ]; then
-            echo "ERROR: Signature file not found: ${sign_path}"
+        if [ ! -f "${sum_path}" ]; then
+            echo "ERROR: Signature file not found: ${sum_path}"
             return 1
         else
-            # TODO: implement signature verify
-            IFS= read -r line <"${sign_path}" || return 1
+            IFS= read -r line <"${sum_path}" || return 1
             expected=${line%%[[:space:]]*}
             if [ -z "${expected}" ]; then
-                echo "ERROR: Bad signature file: ${sign_path}"
+                echo "ERROR: Bad signature file: ${sum_path}"
                 return 1
             fi
         fi
     fi
 
     if [ "${actual}" != "${expected}" ]; then
-        echo "ERROR: SHA256 mismatch for ${file_path}"
+        echo "ERROR: SHA256 mismatch for ${source_path}"
         echo "Expected: ${expected}"
         echo "Actual:   ${actual}"
         return 1
     fi
 
-    echo "SHA256 OK: ${file_path}"
+    echo "SHA256 OK: ${source_path}"
     return 0
 }
 
 # the signature file is just a checksum hash
 signature_file_exists() {
     [ -n "$1" ] || return 1
-    local file_path="$1"
-    local sign_path="$(readlink -f "${file_path}").sum"
-    if [ -f "${sign_path}" ]; then
+    local source_path="$1"
+    local sum_path="$(readlink -f "${source_path}").sum"
+    if [ -f "${sum_path}" ]; then
         return 0
     else
         return 1
@@ -704,6 +704,7 @@ unpack_archive()
 
     local source_path="$1"
     local target_dir="$2"
+    local top_dir="${target_dir%%/*}"
     local dir_tmp=""
 
     if [ ! -d "${target_dir}" ]; then
@@ -711,11 +712,74 @@ unpack_archive()
         trap 'cleanup; exit 130' INT
         trap 'cleanup; exit 143' TERM
         trap 'cleanup' EXIT
-        dir_tmp=$(mktemp -d "${target_dir}.XXXXXX")
+        dir_tmp=$(mktemp -d "${top_dir}.XXXXXX")
         mkdir -p "${dir_tmp}"
         if ! extract_package "${source_path}" "${dir_tmp}"; then
             return 1
         else
+            # try to rename single sub-directory
+            if ! mv -f "${dir_tmp}"/* "${target_dir}"/; then
+                # otherwise, move multiple files and sub-directories
+                mkdir -p "${target_dir}" || return 1
+                mv -f "${dir_tmp}"/* "${target_dir}"/ || return 1
+            fi
+        fi
+        rm -rf "${dir_tmp}" || return 1
+        trap - EXIT INT TERM
+    fi
+
+    return 0
+) # END sub-shell
+
+unpack_and_verify()
+( # BEGIN sub-shell
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+
+    local source_path="$1"
+    local target_dir="$2"
+    local expected="$3"
+    local actual=""
+    local sum_path="$(readlink -f "${source_path}").sum"
+    local line=""
+    local top_dir="${target_dir%%/*}"
+    local dir_tmp=""
+
+    if [ ! -d "${target_dir}" ]; then
+        cleanup() { rm -rf "${dir_tmp}" "${target_dir}"; }
+        trap 'cleanup; exit 130' INT
+        trap 'cleanup; exit 143' TERM
+        trap 'cleanup' EXIT
+        dir_tmp=$(mktemp -d "${top_dir}.XXXXXX")
+        mkdir -p "${dir_tmp}"
+        if ! extract_package "${source_path}" "${dir_tmp}"; then
+            return 1
+        else
+            actual="$(hash_dir "${dir_tmp}")"
+
+            if [ -z "${expected}" ]; then
+                if [ ! -f "${sum_path}" ]; then
+                    echo "ERROR: Signature file not found: ${sum_path}"
+                    return 1
+                else
+                    IFS= read -r line <"${sum_path}" || return 1
+                    expected=${line%%[[:space:]]*}
+                    if [ -z "${expected}" ]; then
+                        echo "ERROR: Bad signature file: ${sum_path}"
+                        return 1
+                    fi
+                fi
+            fi
+
+            if [ "${actual}" != "${expected}" ]; then
+                echo "ERROR: SHA256 mismatch for ${source_path}"
+                echo "Expected: ${expected}"
+                echo "Actual:   ${actual}"
+                return 1
+            fi
+
+            echo "SHA256 OK: ${source_path}"
+
             # try to rename single sub-directory
             if ! mv -f "${dir_tmp}"/* "${target_dir}"/; then
                 # otherwise, move multiple files and sub-directories
@@ -755,6 +819,15 @@ get_latest_package() {
         version=${version%"$suffix"}
         printf '%s\n' "$version"
     )
+    return 0
+}
+
+enable_options() {
+    [ -n "$1" ] || return 1
+    [ -n "$2" ] || return 1
+    local p n
+    $2 && p=enable || p=disable
+    for n in $1; do printf -- "--%s-%s " "$p" "$n"; done
     return 0
 }
 
@@ -812,7 +885,7 @@ update_patch_library() {
 check_static() {
     ldd() {
         if ${ARCH_NATIVE}; then
-            "${SYSROOT}/lib/libc.so" --list "$@"
+            "${PREFIX}/lib/libc.so" --list "$@"
         else
             true
         fi
@@ -892,7 +965,7 @@ add_items_to_install_package()
     local timestamp_file="$1"
     local pkg_files=""
     for fmt in gz xz; do
-        local pkg_file="${PKG_ROOT}_${PKG_ROOT_VERSION}-${PKG_ROOT_RELEASE}_${PKG_TARGET_CPU}.tar.${fmt}"
+        local pkg_file="${PKG_ROOT}_${PKG_ROOT_VERSION}-${PKG_ROOT_RELEASE}_${PKG_TARGET_CPU}${PKG_TARGET_VARIANT}.tar.${fmt}"
         local pkg_path="${CACHED_DIR}/${pkg_file}"
         local temp_path=""
         local timestamp=""
