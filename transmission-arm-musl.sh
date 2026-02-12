8d0391f87a2466737555f7fca71843748cfc2850
Richard Elwell <229418400@users.noreply.github.com>
2026-02-11 22:36:12 -0500

    fix newline not printing

--- transmission-arm-musl.sh
+++ transmission-arm-musl.sh
@@ -1013,7 +1013,7 @@ add_items_to_install_package()
     echo "[*] Finished creating the install package."
     echo ""
     echo "[*] Install package is here:"
-    echo -e "${pkg_files}"
+    printf '%b\n' "${pkg_files}"
     echo ""
 
     return 0
